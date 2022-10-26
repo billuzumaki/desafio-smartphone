ecdb8740aa45faac0782a5ccbceab52e09295ddb
billuzumaki <santos-emiliano@hotmail.com>
2022-10-25 21:13:29 -0300

    first commite

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("283388", "10", "29323228", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("whatsap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("22222", "nokia", "11111", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("telegran");
